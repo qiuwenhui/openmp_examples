c6cdc7efbbeacff4984e47aae5aa2fd74801d363
Q <qiuwenhui1288@qq.com>
2019-02-13 14:09:05 +0800

    explem2

--- example1/section/openmpsection.c
+++ example1/section/openmpsection.c
@@ -11,7 +11,7 @@
 int main(){
     #pragma omp parallel sections // 声明该区域分为若干个 section, section 之间的运行顺序为并行的关系
     {
-    #pragma omp section // 第一个 section, 由某个线程单独完成
+      #pragma omp section // 第一个 section, 由某个线程单独完成
         for (int i = 0; i < 5; ++i){
             printf("%d+\n", i);
         }
---
+++ example2/pi/pi.c
@@ -0,0 +1,25 @@
+/*
+ * @Author: mikey.qiuwenhui 
+ * @Date: 2019-02-13 13:49:53 
+ * @Last Modified by: mikey.qiuwenhui
+ * @Last Modified time: 2019-02-13 14:06:54
+ */
+
+#include <stdio.h>
+#include <time.h>
+static long num_steps = 100000;
+double step;
+void main ()
+{ 
+   float i; double x, pi, sum = 0.0;
+    float start_time,end_time;
+    step = 1.0/(double) num_steps;
+    start_time=clock();
+    for (i=1;i<= num_steps; i++){
+    x = (i-0.5)*step;
+    sum = sum + 4.0/(1.0+x*x);
+    }
+    pi = step * sum;
+    end_time=clock();
+    printf("Pi=%f\nRuning time %f \n",pi,end_time-start_time);
+}
